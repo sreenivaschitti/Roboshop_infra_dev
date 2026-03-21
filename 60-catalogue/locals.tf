b14ef9941e5a45bf6a5263a01fe9f38e31f1a757
sreenivas <sreenivaschitti@gmail.com>
2026-03-21 22:00:13 +0530

    vpc

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -11,5 +11,5 @@ common_tags= {
 
 catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
 private_subnet_id = split(",",  data.aws_ssm_parameter.private_subnet_ids.value)[0]
-vpc_id = data.aws_ssm_parameter.vpc_id
+vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
