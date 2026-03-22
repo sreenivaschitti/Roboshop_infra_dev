ed118bcca6647b1b6094c559059f8f61c278efe4
sreenivas <sreenivaschitti@gmail.com>
2026-03-22 12:14:35 +0530

    vpc

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -89,3 +89,13 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
   security_group_id = local.catalogue_sg_id
 }
 
+# Backend ALB
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.backend_alb_sg_id
+}
